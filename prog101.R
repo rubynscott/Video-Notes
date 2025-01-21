e08be495d782dbb84f8d2332cc45987252721b45
rubynscott <rubynscott@ucsb.edu>
2025-01-21 09:35:34 -0800

    Update prog101.R

--- prog101.R
+++ prog101.R
@@ -9,25 +9,30 @@ library(marinecs100b)
 # Guiding questions -------------------------------------------------------
 
 # What does KEFJ stand for?
-
+#Kenai Fjords which is a region of the northern gulf of Alaska
 # How was temperature monitored?
-
+#the intertidal temperatures were measured by temperature loggers that were placed at each site. Temperature sensors
 # What's the difference between absolute temperature and temperature anomaly?
-
+#absolute temperature is the total and temperature anomaly
 
 # Begin exploring ---------------------------------------------------------
 
-# How many kefj_* vectors are there?
-
+# How many kefj_* vectors are there
+#six kefj vectors
 # How long are they?
-
+#length 2187966
 # What do they represent?
+#kefj_temperature A numeric vector with the temperature readings in Celsius.
+#kefj_site A character vector with the name of the site (one of Aialik, Harris, McCarty, Nuka_Bay, or Nuka_Pass) of the temperature reading.
+#kefj_datetime A POSIXct vector with the date and time of the temperature record.
+#kefj_tidelevel A numeric vector with the predicted tide level (meters) at the time of the temperature reading.
+#kefj_exposure A character vector indicating the type of temperature reading:
 
 # Link to sketch
 
-???_datetime <- kefj_datetime[kefj_site == ???]
-???_interval <- ???_datetime[-1] - ???_datetime[-???]
-t???e(???)
+kefj_datetime <- kefj_datetime[kefj_site == "Nuka_Bay"]
+Nuka_Bay_interval <- kefj_datetime[2:length(kefj_datetime)] - kefj_datetime[1:(length(kefj_datetime)-1)]
+table(Nuka_Bay_interval)
 
 
 # Problem decomposition ---------------------------------------------------
@@ -38,24 +43,35 @@ t???e(???)
 
 # Plot the hottest day
 
-hottest_idx <- ???(kefj_temperature)
-hottest_time <- ???[hottest_idx]
-??? <- kefj_site[???]
-hotday_start <- as.POSIXct("???", tz = "Etc/GMT+8")
-hotday_end <- as.POSIXct("???", tz = "Etc/GMT+8")
-hotday_idx <- ??? == hottest_site &
-  ??? >= hotday_start &
-  ??? <= hotday_end
-hotday_datetime <- ???[hotday_idx]
-hotday_temperature <- ???
-hotday_exposure <- ???
-plot_kefj(???, ???, ???)
+hottest_idx <- which.max(kefj_temperature)
+hottest_time <- kefj_datetime[hottest_idx]
+hottest_site <- kefj_site[hottest_idx]
+hotday_start <- as.POSIXct("2018-07-03 00:00:00", tz = "Etc/GMT+8")
+hotday_end <- as.POSIXct("2018-07-03 23:59:59", tz = "Etc/GMT+8")
+hotday_idx <- kefj_site == hottest_site &
+  kefj_datetime >= hotday_start &
+  kefj_datetime <= hotday_end
+hotday_datetime <- kefj_datetime[hotday_idx]
+hotday_temperature <- kefj_temperature[hotday_idx]
+hotday_exposure <- kefj_exposure[hotday_idx]
+plot_kefj(hotday_datetime, hotday_temperature, hotday_exposure)
 
 # Repeat for the coldest day
-
+coldest_idx <- which.min(kefj_temperature)
+coldest_time <- kefj_datetime[coldest_idx]
+coldest_site <- kefj_site[coldest_idx]
+coldday_start <- as.POSIXct("2013-01-27 00:00:00", tz = "Etc/GMT+8")
+coldday_end <- as.POSIXct("2013-01-27 23:59:59", tz = "Etc/GMT+8")
+coldday_idx <- kefj_site == coldest_site &
+  kefj_datetime >= coldday_start &
+  kefj_datetime <= coldday_end
+coldday_datetime <- kefj_datetime[coldday_idx]
+coldday_temperature <- kefj_temperature[coldday_idx]
+coldday_exposure <- kefj_exposure[coldday_idx]
+plot_kefj(coldday_datetime, coldday_temperature, coldday_exposure)
 # What patterns do you notice in time, temperature, and exposure? Do those
 # patterns match your intuition, or do they differ?
-
+#
 # How did Traiger et al. define extreme temperature exposure?
 
 # Translate their written description to code and calculate the extreme heat
