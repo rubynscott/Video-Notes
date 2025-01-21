81e78b4b005cb7c0cc925cfc7e6a60120a452822
rubynscott <rubynscott@ucsb.edu>
2025-01-21 09:35:17 -0800

    Update prog101-using-functions.R

--- exercises/prog101-using-functions.R
+++ exercises/prog101-using-functions.R
@@ -1,13 +1,15 @@
 # Fill in the blanks so the answers are all 2. Use help for unfamiliar functions
 # if necessary.
-log10(????)
-????(4)
-ceiling(????.1)
+log10(100)
+sqrt(4)
+ceiling(1.1)
 
 # Find the functions for cosine and sine. What are the cosine and sine of 0?
-
+cos(0)
+sin(0)
+2 * cos(0) + sin(0)
 # What do you think the following functions do? Just go off the name, don't look
 # them up.
-read.csv()
-boxplot()
-startsWith()
+read.csv() #reads a csv file
+boxplot() #creates a box and whisker plot with the information entered in parenthesis
+startsWith() #makes something start with the character you enter in parenthasis
